352793d2b7f00af97006c882fe2b6e672a9ed264
Junior Dering <juniordering@hotmail.com>
2022-11-27 18:15:13 -0400

    table component

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-
+  displayedColumns = ['name', 'category'];
+  courses: Course[] = [
+    { _id: 'primeiro', name: 'Angular', category: 'Front End' },
+    { _id: 'primeiro', name: 'Angular', category: 'Front End' },
+  ];
 }
