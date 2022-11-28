ee3eb49653e88d6d620818e494f5ba1125f94859
Junior Dering <juniordering@hotmail.com>
2022-11-27 20:03:54 -0400

    separating imports form Angular Material

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,12 +1,17 @@
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { MatTableModule } from '@angular/material/table';
+
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule
+  ],
 })
 export class CoursesModule {}
