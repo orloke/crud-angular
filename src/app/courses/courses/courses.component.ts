6de2ce3a5235cf3d37e81631cf47181d215e7c82
Junior Dering <juniordering@hotmail.com>
2022-11-28 20:57:57 -0400

    adding dialog error and pipe icons

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -12,8 +14,20 @@ export class CoursesComponent implements OnInit {
   displayedColumns = ['name', 'category'];
   courses$: Observable<Course[]>;
 
-  constructor(private coursesSevice: CoursesService) {
-    this.courses$ = this.coursesSevice.list();
+  constructor(private coursesSevice: CoursesService, public dialog: MatDialog) {
+    this.courses$ = this.coursesSevice.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos.')
+
+        return of([]);
+      })
+    );
+  }
+
+  onError(erroMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: erroMsg
+    });
   }
 
   ngOnInit(): void {}
